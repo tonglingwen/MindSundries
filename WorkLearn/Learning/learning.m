a61b6f55d6f1858b738f5522babb88ba0d350321
tonglw <tonglw@vrp3d.com>
2018-10-11 17:01:14 +0800

    change

--- WorkLearn/Learning/learning.m
+++ WorkLearn/Learning/learning.m
@@ -1,10 +1,13 @@
 syms k1 k2 x y lost;
 syms py vy;
-a=-3.4813624629662863678275495008075;
-b=-43.645600041530581957176024972994;
+a=0.068047106664523317915319945457572;
+b=0.98190121598370927040597659002423;
 learnspeed=0.001;
 
 datemat=load('C:\Users\25285\Desktop\sym\vrk.txt');
+
+plot(datemat(:,1),datemat(:,2),'o')
+
 py=datemat(:,2);
 
 y=k1+k2*x;
@@ -12,22 +15,22 @@ kmat=[k1;k2];
 xmat=py;
 xmat(:,2)=xmat(:,1);
 xmat(:,1)=1;
-
-for i=1:100
 vy=xmat*kmat;
 m=size(py)(1,1);
 lost=1/(2*m)*sum((vy-py).^2);
-vpa(lost);
 k1diff=diff(lost,k1);
 k2diff=diff(lost,k2);
 
+
+#for i=1:20
 tema=a-learnspeed* subs(k1diff,{'k1','k2'},[a,b]);
 temb=b-learnspeed* subs(k2diff,{'k1','k2'},[a,b]);
 a=tema;
 b=temb;
+lostval=vpa(subs(lost,{'k1','k2'},[a,b]))
+#end
 
-lostval=vpa(subs(lost,{'k1','k2'},[a,b]));
-
-vpa(a);
-vpa(b);
-end
+hold on
+plot(datemat(:,1),subs(vy,{'k1','k2'},[a,b]))
+vpa(a)
+vpa(b)
