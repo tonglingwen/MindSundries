6324fc18f59abe38f96937d89799ae11ae1e1b36
tonglw <tonglw@vrp3d.com>
2018-11-07 17:59:18 +0800

    改变

--- WorkLearn/Learning/Practice/logicalpra.m
+++ WorkLearn/Learning/Practice/logicalpra.m
@@ -3,6 +3,9 @@ l0=[0,0,0];
 l1=[0,0,0];
 l2=[0,0,0];
 l3=[0,0,0];
+vk=[5.6358072983815812536564949540352;-0.47826191175764722045477119527677;-2.0099361534564926179219408157877];
+learnspeed=2;
+test=0;
 for i=1:size(l)(1,1)
   if l(i,3)==0
     l0=[l0;l(i,:)];
@@ -17,14 +20,35 @@ for i=1:size(l)(1,1)
     l3=[l3;l(i,:)];
   endif
 endfor
-l0(1,:)=[]
-l1(1,:)=[]
-l2(1,:)=[]
-l3(1,:)=[]
-
-l0y=l0(:,3)
-l1y=l1(:,3)
-l0x=[ones(size(l0)(1,1),1),l0(:,1:2)]
+l0(1,:)=[];
+l1(1,:)=[];
+l2(1,:)=[];
+l3(1,:)=[];
+l123=[l1;l2;l3];
+l123(:,3)=1;
+lall=[l0;l123];
+latwo= lall(:,1:2);
+xmax= max(latwo);
+xmin= min(latwo);
+distance =20./(xmax-xmin);
+latwo= latwo.*distance.- (xmax.*distance.-10);
+lax=[ones(size(lall)(1,1),1),latwo];
+lay=lall(:,3);
 
 syms k0 k1 k2;
-pm=k0+k1+k2;
+k=[k0;k1;k2];
+lak= lax*k;
+h=1./(1.+e.^(0.-lak));
+J=(-1/size(lax)(1,1))*sum(lay.*log(h).+(1.-lay).*log(1.-h));
+k0diff=diff(J,k0);
+k1diff=diff(J,k1);
+k2diff=diff(J,k2);
+kdiff=[k0diff;k1diff;k2diff];
+for i=1:50
+diffv=vpa(subs(kdiff,{'k0','k1','k2'},vk'));
+vk=vk-learnspeed*diffv;
+if mod(i,5)==0
+  lostval=vpa(subs(J,{'k0','k1','k2'},vk'))
+endif
+endfor
+vpa(vk)
