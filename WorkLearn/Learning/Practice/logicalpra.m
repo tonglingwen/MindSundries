bb8ccecac9a33c669cd9a77324d95d9e350a4ea9
tonglingwen <2528514903@qq.com>
2018-11-10 19:51:50 +0800

    改变

--- WorkLearn/Learning/Practice/logicalpra.m
+++ WorkLearn/Learning/Practice/logicalpra.m
@@ -1,9 +1,24 @@
+mod=load('C:\Users\25285\Desktop\ss\model.mat')
+ltest=load('C:\Users\25285\Desktop\ss\test.txt');
+testvk=mod.vk;
+testdistance=mod.expend(2,:);
+testmax=mod.expend(1,:);
+ltestx= ltest(:,1:2).*testdistance.- (testmax.*testdistance.-10);
+ltestx=[ones(size(ltestx)(1,1),1),ltestx];
+r= [1./(1.+e.^(0.-ltestx*testvk)),ltest(:,3)];
+r
+
+return
+
 l=load('C:\Users\25285\Desktop\ss\save.txt');
+
+
 l0=[0,0,0];
 l1=[0,0,0];
 l2=[0,0,0];
 l3=[0,0,0];
-vk=[5.6358072983815812536564949540352;-0.47826191175764722045477119527677;-2.0099361534564926179219408157877];
+vk=[5.3545670646381196181146543186266;-0.17043546962693495880512976396681;-2.2285825145779419953529928022823];
+
 learnspeed=2;
 test=0;
 for i=1:size(l)(1,1)
@@ -34,7 +49,7 @@ distance =20./(xmax-xmin);
 latwo= latwo.*distance.- (xmax.*distance.-10);
 lax=[ones(size(lall)(1,1),1),latwo];
 lay=lall(:,3);
-
+expend=[xmax;distance];
 syms k0 k1 k2;
 k=[k0;k1;k2];
 lak= lax*k;
@@ -52,3 +67,4 @@ if mod(i,5)==0
 endif
 endfor
 vpa(vk)
+save 'C:\Users\25285\Desktop\ss\model.mat' vk expend
