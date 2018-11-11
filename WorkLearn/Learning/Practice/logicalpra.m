b35e5778a4253992e24e8eb7aa5a81ad134e417a
tonglingwen <2528514903@qq.com>
2018-11-11 22:32:07 +0800

    改变

--- WorkLearn/Learning/Practice/logicalpra.m
+++ WorkLearn/Learning/Practice/logicalpra.m
@@ -1,14 +1,17 @@
-mod=load('C:\Users\25285\Desktop\ss\model.mat')
+trainmod=load('C:\Users\25285\Desktop\ss\model.mat')
+test=0;
+
+if test==1
 ltest=load('C:\Users\25285\Desktop\ss\test.txt');
-testvk=mod.vk;
-testdistance=mod.expend(2,:);
-testmax=mod.expend(1,:);
+testvk=trainmod.vk;
+testdistance=trainmod.expend(2,:);
+testmax=trainmod.expend(1,:);
 ltestx= ltest(:,1:2).*testdistance.- (testmax.*testdistance.-10);
 ltestx=[ones(size(ltestx)(1,1),1),ltestx];
 r= [1./(1.+e.^(0.-ltestx*testvk)),ltest(:,3)];
 r
-
 return
+endif
 
 l=load('C:\Users\25285\Desktop\ss\save.txt');
 
@@ -17,10 +20,12 @@ l0=[0,0,0];
 l1=[0,0,0];
 l2=[0,0,0];
 l3=[0,0,0];
-vk=[5.3545670646381196181146543186266;-0.17043546962693495880512976396681;-2.2285825145779419953529928022823];
+vk=vpa(trainmod.vk)
+#[5.3545670646381196181146543186266;-0.17043546962693495880512976396681;-2.2285825145779419953529928022823];
 
 learnspeed=2;
-test=0;
+punish=1;
+
 for i=1:size(l)(1,1)
   if l(i,3)==0
     l0=[l0;l(i,:)];
@@ -54,12 +59,12 @@ syms k0 k1 k2;
 k=[k0;k1;k2];
 lak= lax*k;
 h=1./(1.+e.^(0.-lak));
-J=(-1/size(lax)(1,1))*sum(lay.*log(h).+(1.-lay).*log(1.-h));
+J=(-1/size(lax)(1,1))*sum(lay.*log(h).+(1.-lay).*log(1.-h))+(punish/(2*size(lax)(1,1)))*sum(k.*k);
 k0diff=diff(J,k0);
 k1diff=diff(J,k1);
 k2diff=diff(J,k2);
 kdiff=[k0diff;k1diff;k2diff];
-for i=1:50
+for i=1:100
 diffv=vpa(subs(kdiff,{'k0','k1','k2'},vk'));
 vk=vk-learnspeed*diffv;
 if mod(i,5)==0
