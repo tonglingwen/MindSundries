3c14c256c0f85b2678e8ea5148b37e66cd618586
tonglingwen <2528514903@qq.com>
2018-12-16 23:14:38 +0800

    1

--- WorkLearn/Learning/Python/Operator/extract.py
+++ WorkLearn/Learning/Python/Operator/extract.py
@@ -1,12 +1,12 @@
-#将指定文件夹path下面的所有zip、rar、7z通过WinRAR解压到指定目录下
+
 import os
 import os.path
-path = 'E:/model'              #要进行解压缩的目录
+path = 'E:/model'
 dirs = os.listdir(path)
 for dir in dirs:
  extent=os.path.splitext(dir)[1].replace('.','');
  file=os.path.splitext(dir)[0];
  print(extent)
- if extent=='zip'or extent=='rar'or extent=='7z': #判断后缀名是否是这些格式       
-  os.makedirs(path+"/"+file+extent)               #创建文件夹 
-  os.system('WinRAR e '+path+"/"+"\""+dir+"\" "+path+"/"+"\""+file+extent+"\"") #启动WinRAR程序进行解压缩
+ if extent=='zip'or extent=='rar'or extent=='7z':
+  os.makedirs(path+"/"+file+extent) 
+  os.system('WinRAR e '+path+"/"+"\""+dir+"\" "+path+"/"+"\""+file+extent+"\"")
