0d31770cfc7ce6054a1ef0c93862572c65860592
tonglw <tonglw@vrp3d.com>
2019-03-07 16:11:04 +0800

    imagenet read

--- WorkLearn/Learning/Tensorflow/Tensorflow_example/imageNetLoad/Main.py
+++ WorkLearn/Learning/Tensorflow/Tensorflow_example/imageNetLoad/Main.py
@@ -3,14 +3,15 @@ import tensorflow as tf
 import numpy as np  
 import os
 import matplotlib.pyplot as plt
+import time
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 np.set_printoptions(threshold=np.inf)  
 
-dataset = readImageNet.ImageNetDataSet('F:/ILSVRC2012_dataset/image_train')
+dataset = readImageNet.ImageNetDataSet('C:/Users/25285/Desktop/tar')
 #image_raw_data_jpg = tf.gfile.FastGFile('n01491361_25.JPEG', 'rb').read()
 #image_raw_data_jpg = tf.image.decode_jpeg(image_raw_data_jpg)
-dataset.get_labels('train_label_origin.txt')
+dataset.get_labels('train_label_test.txt')
 image_batch,label_batch = dataset.get_batch_data()
 #print(dataset.trainLabel)
 sess = tf.Session()
@@ -20,13 +21,14 @@ coord = tf.train.Coordinator()
 threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 #print(sess.run(dataset.input_queue))
 #print(sess.run(dataset.trainLabel))
-#for i in range(10):
-image=sess.run(dataset.imagess)
-#print(image)
-#image = sess.run(image_data)
-plt.imshow(image)
-plt.show()
-print(image.shape)
+for i in range(10):
+	image_v,label_v=sess.run([image_batch,label_batch])
+	image_v = tf.reshape(image_v,[-1,227,227,3])
+	image_v= image_v.eval(session=sess)
+	for image_sub in image_v:
+		plt.imshow(image_sub)
+		plt.draw()
+		plt.pause(0.01)
 #print(sess.run(image_raw_data_jpg).shape)
 coord.request_stop()
 coord.join(threads)
