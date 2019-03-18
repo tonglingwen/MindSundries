838b8fd513f2fc08aaed0cd7189bebc6b4f5dba6
tonglingwen <2528514903@qq.com>
2019-03-18 10:46:56 +0800

    resnet train

--- WorkLearn/Learning/Tensorflow/Tensorflow_example/ResNet/load.py
+++ WorkLearn/Learning/Tensorflow/Tensorflow_example/ResNet/load.py
@@ -1,5 +1,23 @@
 import tensorflow as tf
+import readImageNet
+from scipy.misc import imread,imresize
 
+def batch_norm(inputs,is_training,is_conv_out=True,decay=0.999):
+	scale=tf.Variable(tf.ones([inputs.get_shape()[-1]]))
+	beta=tf.Variable(tf.zeros([inputs.get_shape()[-1]]))
+	pop_mean=tf.Variable(tf.zeros([inputs.get_shape()[-1]]),trainable=False)
+	pop_var=tf.Variable(tf.ones([inputs.get_shape()[-1]]),trainable=False)
+	if is_training:
+		if is_conv_out:
+			batch_mean,batch_var=tf.nn.moments(inputs,[0,1,2])
+		else:
+			batch_mean,batch_var=tf.nn.moments(inputs,[0])
+		train_mean=tf.assign(pop_mean,pop_mean*decay+batch_mean*(1-decay))
+		train_var=tf.assign(pop_var,pop_var*decay+batch_var*(1-decay))
+		with tf.control_dependencies([train_mean,train_var]):
+			return tf.nn.batch_normalization(inputs,batch_mean,batch_var,beta,scale,0.001)
+	else:
+		return tf.nn.batch_normalization(inputs,pop_mean,pop_var,beta,scale,0.001)
 
 def residual_2(input,out_channel):
 	channel =int(input.get_shape()[3])
@@ -7,17 +25,22 @@ def residual_2(input,out_channel):
 	w_conv=tf.Variable(tf.truncated_normal([3,3,channel,out_channel[0]],stddev=0.0001))
 	b_conv=tf.Variable(tf.constant(0.1,shape=[out_channel[0]]))
 	if isChange:
-		h_conv=tf.nn.relu(tf.nn.conv2d(input, w_conv, strides=[1, 2, 2, 1], padding='SAME')+b_conv)
+		h_conv=tf.nn.conv2d(input, w_conv, strides=[1, 2, 2, 1], padding='SAME')+b_conv
 	else:
-		h_conv=tf.nn.relu(tf.nn.conv2d(input, w_conv, strides=[1, 1, 1, 1], padding='SAME')+b_conv)
+		h_conv=tf.nn.conv2d(input, w_conv, strides=[1, 1, 1, 1], padding='SAME')+b_conv
+	batch_norm(h_conv,True)
+	h_conv= tf.nn.relu(h_conv)
 	w_conv1=tf.Variable(tf.truncated_normal([3,3,out_channel[0],out_channel[1]],stddev=0.0001))
 	b_conv1=tf.Variable(tf.constant(0.1,shape=[out_channel[1]]))
-	h_conv1=tf.nn.relu(tf.nn.conv2d(h_conv, w_conv1, strides=[1, 1, 1, 1], padding='SAME')+b_conv1)
-	
+	h_conv1=tf.nn.conv2d(h_conv, w_conv1, strides=[1, 1, 1, 1], padding='SAME')+b_conv1
+	h_conv1= batch_norm(h_conv1,True)
+	h_conv1= tf.nn.relu(h_conv1)
 	if isChange:
-		w_conv2=tf.Variable(tf.truncated_normal([1,1,channel,out_channel[1]],stddev=0.0001))
-		#b_conv2=tf.Variable(tf.constant(0.1,shape=[out_channel[1]]))
-		input=tf.nn.conv2d(input,w_conv2,strides=[1,2,2,1],padding='SAME')#+b_conv2
+		w_conv2=tf.Variable(tf.truncated_normal([3,3,channel,out_channel[1]],stddev=0.0001))
+		b_conv2=tf.Variable(tf.constant(0.1,shape=[out_channel[1]]))
+		input=tf.nn.conv2d(input,w_conv2,strides=[1,2,2,1],padding='SAME')+b_conv2
+		input=batch_norm(input,True)
+		input=tf.nn.relu(input)
 	return h_conv1+input
 	
 
@@ -38,18 +61,35 @@ def residual_3(input,out_channel):
 	h_conv2=tf.nn.relu(tf.nn.conv2d(h_conv1,w_conv2,strides=[1,1,1,1],padding='SAME')+b_conv2)
 	
 	if isChange:
-		w_conv3=tf.Variable(tf.truncated_normal([1,1,channel,out_channel[2]],stddev=0.0001))
+		w_conv3=tf.Variable(tf.truncated_normal([3,3,channel,out_channel[2]],stddev=0.0001))
 		#b_conv3=tf.Variable(tf.constant(0.1,shape=[out_channel[1]]))
 		input=tf.nn.conv2d(input,w_conv3,strides=[1,2,2,1],padding='SAME')#+b_conv3
 	return h_conv2+input
 
-input_d=tf.placeholder('float',[None,224,224,3])
+
+ClassNum=1000
+ImagePath='F:/ILSVRC2012_dataset/image_train'
+LabelPath='train_label.txt'
+SavePath='./model/AlexNetModel.ckpt'
+BatchSize=50
+
+	
+dataset = readImageNet.ImageNetDataSet(ImagePath,ClassNum,BatchSize)#加载图片根目录
+dataset.get_labels(LabelPath)
+image_batch,label_batch = dataset.get_batch_data()
+
+	
+x = tf.placeholder("float", [None, 224*224*3])
+y_ = tf.placeholder("float", [None,ClassNum])
+
+input_d=tf.reshape(x,[-1,224,224,3])
+
 w_conv=tf.Variable(tf.truncated_normal([7,7,3,64],stddev=0.0001))
 b_conv=tf.constant(0.1,shape=[64])
 h_conv=tf.nn.relu(tf.nn.conv2d(input_d,w_conv,strides=[1,2,2,1],padding='SAME')+b_conv)
 h_pool1=tf.nn.max_pool(h_conv, ksize=[1, 3, 3, 1],strides=[1, 2, 2, 1], padding='SAME')
 
-'''
+
 h_residual1=residual_2(h_pool1,[64,64])
 h_residual2=residual_2(h_residual1,[64,64])
 h_residual3=residual_2(h_residual2,[64,64])
@@ -69,8 +109,8 @@ h_residual13=residual_2(h_residual12,[256,256])
 h_residual13=residual_2(h_residual12,[512,512])
 h_residual14=residual_2(h_residual13,[512,512])
 h_residual15=residual_2(h_residual14,[512,512])
-'''
 
+'''
 h_residual1=residual_3(h_pool1,[64,64,64])
 h_residual2=residual_3(h_residual1,[64,64,64])
 h_residual3=residual_3(h_residual2,[64,64,64])
@@ -90,21 +130,34 @@ h_residual13=residual_3(h_residual12,[256,256,256])
 h_residual13=residual_3(h_residual12,[512,512,512])
 h_residual14=residual_3(h_residual13,[512,512,512])
 h_residual15=residual_3(h_residual14,[512,512,512])
-
+'''
 h_pool2=tf.nn.avg_pool(h_residual15, ksize=[1, 7, 7, 1],strides=[1, 1, 1, 1], padding='VALID')
 
-h_fc=tf.reshape(h_pool2,[-1,512])
+h_fc=tf.reshape(h_pool2,[-1,1*1*512])
 
-w_fc=tf.Variable(tf.truncated_normal([512,1000],stddev=0.01))
+w_fc=tf.Variable(tf.truncated_normal([1*1*512,1000],stddev=0.01))
 b_fc=tf.Variable(tf.constant(0.1,shape=[1000]))
-h_fc=tf.matmul(h_fc,w_fc)+w_fc
+h_fc=tf.matmul(h_fc,w_fc)+b_fc
 
+cross_entropy =tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=h_fc,labels=y_))# -tf.reduce_sum(y_*tf.log(y_conv_clip))#
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)#tf.train.GradientDescentOptimizer(1e-4).minimize(cross_entropy)#
 
-
-print(h_fc)
-
-
-
+saver=tf.train.Saver()#保存模型
+sess=tf.Session()
+sess.run(tf.global_variables_initializer())
+sess.run(tf.local_variables_initializer())
+#save_model=tf.train.latest_checkpoint('.//model')
+#saver.restore(sess,save_model)
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+for i in range(1):
+	image_v,label_v=sess.run([image_batch,label_batch])
+	train_step_out,cross_entropy_out=sess.run([train_step,cross_entropy],feed_dict={x:image_v,y_:label_v})
+	print("cross_entropy",cross_entropy_out)
+saver=tf.train.Saver()
+saver.save(sess,SavePath)
+coord.request_stop()
+coord.join(threads)
 
 
 
---
+++ WorkLearn/Learning/Tensorflow/Tensorflow_example/ResNet/readImageNet.py
@@ -0,0 +1,34 @@
+import os
+import tensorflow as tf
+
+class ImageNetDataSet:
+	#trainLabel=[]
+	#trainLabel_value=[]
+
+	def __init__(self,rootpath,classnum,batchsize):
+		self.trainLabel = []
+		self.trainLabel_value=[]
+		self.rootpath=rootpath
+		self.classnum=classnum
+		self.batchsize=batchsize
+	
+	def get_labels(self,path):
+		for line in open(path):
+			strlist=line.split(' ')
+			self.trainLabel.append(strlist[0].replace(' ',''))
+			self.trainLabel_value.append(int(strlist[1]))
+		self.trainLabel = tf.convert_to_tensor(self.trainLabel)
+		self.trainLabel_value=tf.convert_to_tensor(self.trainLabel_value)
+		self.input_image,self.input_label = tf.train.slice_input_producer([self.trainLabel, self.trainLabel_value], shuffle=True,num_epochs=None)
+		self.images=tf.image.decode_jpeg(tf.read_file(self.rootpath+'/'+self.input_image),channels=3)
+		#self.images=tf.image.convert_image_dtype(tf.image.decode_jpeg(tf.read_file(self.rootpath+'/'+self.input_image),channels=3),tf.float32)
+		#self.images=tf.image.resize_image_with_crop_or_pad(self.images,224,224)
+		self.images =tf.image.resize_images(self.images, size=[224, 224])
+		self.images=tf.image.per_image_standardization(self.images)
+		self.images=tf.reshape(self.images,[224*224*3])
+
+
+	def get_batch_data(self):
+		image_batch, label_batch = tf.train.batch([self.images,self.input_label], batch_size=self.batchsize, num_threads=64, capacity=2048,allow_smaller_final_batch=True)
+		label_batch = tf.one_hot(label_batch,self.classnum,1.0,0.0,dtype=tf.float32)
+		return image_batch,label_batch
