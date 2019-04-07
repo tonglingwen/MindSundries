0a19195c4306db4cc77aa1c898b813ba062a50c3
tonglingwen <2528514903@qq.com>
2019-04-07 16:21:13 +0800

    WorkLearn\Learning\Tensorflow\Tensorflow_example\SPPNet\sppnet.py

--- WorkLearn/Learning/Tensorflow/Tensorflow_example/SPPNet/sppnet.py
+++ WorkLearn/Learning/Tensorflow/Tensorflow_example/SPPNet/sppnet.py
@@ -1,5 +1,24 @@
 import tensorflow as tf
 
-def sppnet(data,outdims):
-	
-	return ""
+def sppnet(data,pyramid=[3,2,1]):
+	result=[]
+	w=tf.to_float(data.shape[1])
+	h=tf.to_float(data.shape[2])
+	dims=tf.to_int32(data.shape[3])
+	for i in pyramid:
+		wi=w/tf.to_float(i)
+		hi=w/tf.to_float(i)
+		wksize=tf.to_int32(tf.math.ceil(wi))
+		wstrides=tf.math.floor(wi)
+		hksize=tf.math.ceil(hi)
+		hstrides=tf.math.floor(hi)
+		h_pool2=tf.nn.avg_pool(data, ksize=[1, wksize, hksize, 1],strides=[1, wstrides, hstrides, 1], padding='VALID')
+		h_pool2=tf.reshape(h_pool2,[-1,dims*i*i])
+		tf.concat([result,h_pool2],0)
+	return result
+
+
+
+x = tf.placeholder("float", [None, 276,657,3])
+
+print(sppnet(x))
