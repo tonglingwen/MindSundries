91dfa6044eccf979c8fe2d3bf9f6fc56d7569e4d
tonglw <tonglw@vrp3d.com>
2019-04-08 11:18:42 +0800

    update sppnet

--- WorkLearn/Learning/Tensorflow/Tensorflow_example/SPPNet/sppnet.py
+++ WorkLearn/Learning/Tensorflow/Tensorflow_example/SPPNet/sppnet.py
@@ -1,24 +1,31 @@
 import tensorflow as tf
+import math
+import numpy as np
 
 def sppnet(data,pyramid=[3,2,1]):
 	result=[]
-	w=tf.to_float(data.shape[1])
-	h=tf.to_float(data.shape[2])
-	dims=tf.to_int32(data.shape[3])
+	shape= data.get_shape().as_list()
+	w=shape[1]
+	h=shape[2]
+	dims=shape[3]
+	index=0
 	for i in pyramid:
-		wi=w/tf.to_float(i)
-		hi=w/tf.to_float(i)
-		wksize=tf.to_int32(tf.math.ceil(wi))
-		wstrides=tf.math.floor(wi)
-		hksize=tf.math.ceil(hi)
-		hstrides=tf.math.floor(hi)
+		wi=w/i
+		hi=h/i
+		wksize=math.ceil(wi)
+		wstrides=math.floor(wi)
+		hksize=math.ceil(hi)
+		hstrides=math.floor(hi)
 		h_pool2=tf.nn.avg_pool(data, ksize=[1, wksize, hksize, 1],strides=[1, wstrides, hstrides, 1], padding='VALID')
 		h_pool2=tf.reshape(h_pool2,[-1,dims*i*i])
-		tf.concat([result,h_pool2],0)
+		if index==0:
+			result=h_pool2
+		else:
+			result=tf.concat([result,h_pool2],1)
+		index=1
 	return result
 
 
-
-x = tf.placeholder("float", [None, 276,657,3])
+x = tf.placeholder("float", [None, 3,3,512])
 
 print(sppnet(x))
