1326eced8276b1e35dd93a45e0514c7841838c20
tonglw <tonglw@vrp3d.com>
2019-05-10 17:52:15 +0800

    WorkLearn\Learning\Tensorflow\Tensorflow_example\Voc2010Reader\reader.py

--- WorkLearn/Learning/Tensorflow/Tensorflow_example/Voc2010Reader/reader.py
+++ WorkLearn/Learning/Tensorflow/Tensorflow_example/Voc2010Reader/reader.py
@@ -6,12 +6,20 @@ def GetBBoxByPath(path):
 	domobj = xmldom.parse(xmlfilepath)
 	elementobj = domobj.documentElement
 	bndbox=elementobj.getElementsByTagName("object")[0].getElementsByTagName("bndbox")[0]
+	name=elementobj.getElementsByTagName("object")[0].getElementsByTagName("name")[0]
+	size=len(bndbox.childNodes)
+	for node in bndbox.childNodes:
+		print(node)
+	print("name",name.firstChild.data)
+	return GetBndbox(bndbox)
+
+def GetBndbox(bndbox):
 	xmin=int(bndbox.getElementsByTagName("xmin")[0].firstChild.data)
 	ymin=int(bndbox.getElementsByTagName("ymin")[0].firstChild.data)
 	xmax=int(bndbox.getElementsByTagName("xmax")[0].firstChild.data)
 	ymax=int(bndbox.getElementsByTagName("ymax")[0].firstChild.data)
 	return [xmin,ymin,xmax,ymax]
 
-'''
-print(GetBBoxByPath(r'F:\ILSVRC2012_dataset\image_other\ILSVRC_train_v2\n01440764\n01440764_18.xml'))
-'''
+
+
+print(GetBBoxByPath("C:\\Users\\25285\\Desktop\\voc\\VOCtrainval_11-May-2012\\VOCdevkit\\VOC2012\\Annotations\\2007_000027.xml"))
